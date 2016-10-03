d29acd8a7f7d6860009256a0035d185c2a2b4d49
Rodrigo Moutinho <rcm1989@gmail.com>
2016-10-03 07:55:07 -0300

    Controlando os estilos de um elemento.

--- src/main/java/br/com/rcmoutinho/javatohtml/core/ElementStyle.java
+++ src/main/java/br/com/rcmoutinho/javatohtml/core/ElementStyle.java
@@ -3,6 +3,8 @@ package br.com.rcmoutinho.javatohtml.core;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Representa todos os estilos de um elemento.
  * 
@@ -43,5 +45,34 @@ public class ElementStyle {
 	 */
 	protected boolean isEmpty() {
 		return this.style.isEmpty();
+	}
+	
+	/**
+	 * Adiciona o estilo desejado.
+	 * 
+	 * @param property
+	 * @param value
+	 * @return
+	 */
+	public ElementStyle add(String property, String value) {
+		
+		if (StringUtils.isNotBlank(property))
+			this.style.put(property, value); // TODO Verificar como tratar valores já existentes no estilo
+		
+		return this;
+	}
+	
+	/**
+	 * Remove o estilo desejado.
+	 * 
+	 * @param property
+	 * @return
+	 */
+	public ElementStyle remove(String property) {
+		
+		if (StringUtils.isNotBlank(property) && this.style.get(property)!=null)
+			this.style.remove(property);
+		
+		return this;
 	}
 }
