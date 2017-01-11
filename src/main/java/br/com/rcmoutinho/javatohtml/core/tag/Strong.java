1d661266736cd2d68cbea20cf4e0ef7a8cace48e
Rodrigo Moutinho <rcm1989@gmail.com>
2017-01-11 08:05:55 -0200

    Suporte para a tag <strong>.

---
+++ src/main/java/br/com/rcmoutinho/javatohtml/core/tag/Strong.java
@@ -0,0 +1,34 @@
+package br.com.rcmoutinho.javatohtml.core.tag;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import br.com.rcmoutinho.javatohtml.core.Element;
+
+/**
+ * Implementação da tag HTML &lt;strong&gt;. <br>
+ * <br>
+ * Ref.: http://www.w3schools.com/tags/tag_strong.asp
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 11 de jan de 2017
+ * @email rcm1989@gmail.com
+ */
+public class Strong extends Element<Strong> {
+
+	@Override
+	protected String getName() {
+		return "strong";
+	}
+
+	@Override
+	protected Map<String, String> getSpecificAttributesMap() {
+		return new LinkedHashMap<String, String>();
+	}
+
+	@Override
+	protected Strong getType() {
+		return this;
+	}
+}
