6329d3c121baa951d673fda93a426969dad3f5a9
Rodrigo Moutinho <rcm1989@gmail.com>
2017-01-11 08:07:00 -0200

    Suporte para a tag <hr>.

---
+++ src/main/java/br/com/rcmoutinho/javatohtml/core/tag/Hr.java
@@ -0,0 +1,34 @@
+package br.com.rcmoutinho.javatohtml.core.tag;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import br.com.rcmoutinho.javatohtml.core.Element;
+
+/**
+ * Implementação da tag HTML &lt;hr&gt;. <br>
+ * <br>
+ * Ref.: http://www.w3schools.com/tags/tag_hr.asp
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 11 de jan de 2017
+ * @email rcm1989@gmail.com
+ */
+public class Hr extends Element<Hr> {
+
+	@Override
+	protected String getName() {
+		return "hr";
+	}
+
+	@Override
+	protected Map<String, String> getSpecificAttributesMap() {
+		return new LinkedHashMap<String, String>();
+	}
+
+	@Override
+	protected Hr getType() {
+		return this;
+	}
+}
