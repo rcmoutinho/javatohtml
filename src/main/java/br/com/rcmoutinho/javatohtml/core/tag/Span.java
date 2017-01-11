f267de88e8309391f9afeded78eb9f4d43a5276a
Rodrigo Moutinho <rcm1989@gmail.com>
2017-01-11 08:05:44 -0200

    Suporte para a tag <span>.

---
+++ src/main/java/br/com/rcmoutinho/javatohtml/core/tag/Span.java
@@ -0,0 +1,34 @@
+package br.com.rcmoutinho.javatohtml.core.tag;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import br.com.rcmoutinho.javatohtml.core.Element;
+
+/**
+ * Implementação da tag HTML &lt;span&gt;. <br>
+ * <br>
+ * Ref.: http://www.w3schools.com/tags/tag_span.asp
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 11 de jan de 2017
+ * @email rcm1989@gmail.com
+ */
+public class Span extends Element<Span> {
+
+	@Override
+	protected String getName() {
+		return "span";
+	}
+
+	@Override
+	protected Map<String, String> getSpecificAttributesMap() {
+		return new LinkedHashMap<String, String>();
+	}
+
+	@Override
+	protected Span getType() {
+		return this;
+	}
+}
