a81c9974cd22b0288f6d36b2b3dbfc1762d6f4a6
Rodrigo Moutinho <rcm1989@gmail.com>
2017-01-11 08:05:34 -0200

    Suporte para a tag <p>.

---
+++ src/main/java/br/com/rcmoutinho/javatohtml/core/tag/P.java
@@ -0,0 +1,34 @@
+package br.com.rcmoutinho.javatohtml.core.tag;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import br.com.rcmoutinho.javatohtml.core.Element;
+
+/**
+ * Implementação da tag HTML &lt;p&gt;. <br>
+ * <br>
+ * Ref.: http://www.w3schools.com/tags/tag_p.asp
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 11 de jan de 2017
+ * @email rcm1989@gmail.com
+ */
+public class P extends Element<P> {
+
+	@Override
+	protected String getName() {
+		return "p";
+	}
+
+	@Override
+	protected Map<String, String> getSpecificAttributesMap() {
+		return new LinkedHashMap<String, String>();
+	}
+
+	@Override
+	protected P getType() {
+		return this;
+	}
+}
