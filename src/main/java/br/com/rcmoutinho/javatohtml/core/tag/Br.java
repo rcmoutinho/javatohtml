fa82f59d9916c8f42104616e4d7640b7a3b5c196
Rodrigo Moutinho <rcm1989@gmail.com>
2017-01-11 08:02:54 -0200

    Suporte para a tag <br>.

---
+++ src/main/java/br/com/rcmoutinho/javatohtml/core/tag/Br.java
@@ -0,0 +1,34 @@
+package br.com.rcmoutinho.javatohtml.core.tag;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import br.com.rcmoutinho.javatohtml.core.Element;
+
+/**
+ * Implementação da tag HTML &lt;br&gt;. <br>
+ * <br>
+ * Ref.: http://www.w3schools.com/tags/tag_br.asp
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 11 de jan de 2017
+ * @email rcm1989@gmail.com
+ */
+public class Br extends Element<Br> {
+
+	@Override
+	protected String getName() {
+		return "br";
+	}
+
+	@Override
+	protected Map<String, String> getSpecificAttributesMap() {
+		return new LinkedHashMap<String, String>();
+	}
+
+	@Override
+	protected Br getType() {
+		return this;
+	}
+}
