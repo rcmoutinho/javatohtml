a1337a5160df2308e7a758028d174624a3c2181f
Rodrigo Moutinho <rcm1989@gmail.com>
2017-01-26 07:49:24 -0200

    Unit test for <h1>

---
+++ src/test/java/br/com/rcmoutinho/javatohtml/core/tag/H1Test.java
@@ -0,0 +1,75 @@
+package br.com.rcmoutinho.javatohtml.core.tag;
+
+import static br.com.rcmoutinho.javatohtml.core.ElementTestUtil.countSupportedTagToAppend;
+import static br.com.rcmoutinho.javatohtml.core.ElementTestUtil.countSupportedTagToPrepend;
+import static br.com.rcmoutinho.javatohtml.core.ElementTestUtil.countUnsupportedTagExceptionToAppend;
+import static br.com.rcmoutinho.javatohtml.core.ElementTestUtil.countUnsupportedTagExceptionToPrepend;
+import static br.com.rcmoutinho.javatohtml.core.ElementTestUtil.testStringToAppend;
+import static br.com.rcmoutinho.javatohtml.core.ElementTestUtil.testStringToPrepend;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.rcmoutinho.javatohtml.core.Element;
+import br.com.rcmoutinho.javatohtml.core.ElementUtils;
+
+/**
+ * Unit test for {@link H1}.
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 26 de jan de 2017
+ * @email rcm1989@gmail.com
+ */
+public class H1Test {
+
+	private Class<? extends Element<?>> implClass;
+	private List<Class<? extends Element<?>>> supportedElements;
+	private List<Class<? extends Element<?>>> notSupportedElements;
+
+	@Before
+	public void beforeTesting() {
+		this.implClass = H1.class;
+		this.supportedElements = new H1().getSupportedElements();
+
+		this.notSupportedElements = ElementUtils.getAllImplementedElements();
+		this.notSupportedElements.removeAll(this.supportedElements);
+	}
+
+	@Test
+	public void checkSupportedElementsToAppend() {
+		int supportedTagCount = countSupportedTagToAppend(this.implClass, this.supportedElements);
+		assertEquals(supportedTagCount, this.supportedElements.size());
+	}
+
+	@Test
+	public void checkSupportedElementsToPrepend() {
+		int supportedTagCount = countSupportedTagToPrepend(this.implClass, this.supportedElements);
+		assertEquals(supportedTagCount, this.supportedElements.size());
+	}
+
+	@Test
+	public void checkSupportedStringToAppend() {
+		testStringToAppend(this.implClass);
+	}
+
+	@Test
+	public void checkSupportedStringToPrepend() {
+		testStringToPrepend(this.implClass);
+	}
+
+	@Test
+	public void checkUnsupportedElementsToAppend() {
+		int unsupportedTagCount = countUnsupportedTagExceptionToAppend(this.implClass, this.notSupportedElements);
+		assertEquals(unsupportedTagCount, this.notSupportedElements.size());
+	}
+
+	@Test
+	public void checkUnsupportedElementsToPrepend() {
+		int unsupportedTagCount = countUnsupportedTagExceptionToPrepend(this.implClass, this.notSupportedElements);
+		assertEquals(unsupportedTagCount, this.notSupportedElements.size());
+	}
+}
