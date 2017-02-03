1f1cc2d9e8d2f4125d0e0baf5ce7f9da63903c8f
Rodrigo Moutinho <rcm1989@gmail.com>
2017-02-03 08:27:59 -0200

    More unit tests for Element

--- src/test/java/br/com/rcmoutinho/javatohtml/core/ElementTest.java
+++ src/test/java/br/com/rcmoutinho/javatohtml/core/ElementTest.java
@@ -20,12 +20,6 @@ public class ElementTest {
 	private Tag tag;
 	private String tagName;
 
-	@Before
-	public void beforeTesting() {
-		this.tagName = "tag";
-		this.tag = new Tag(this.tagName);
-	}
-
 	@Test
 	public void addingDataAttrInOrder() {
 
@@ -33,5 +27,81 @@ public class ElementTest {
 
 		String html = "<" + this.tagName + " data-test1=\"value1\" data-test2=\"value2\"></" + this.tagName + ">";
 		assertEquals(html, this.tag.toHtml());
+	}
+
+	@Test
+	public void appendInvalidValuesGenerateSimpleTag() {
+
+		String string = null;
+		this.tag.append(string);
+		this.tag.append("");
+
+		Element<?> element = null;
+		this.tag.append(element);
+		this.tag.appendTo(element);
+
+		assertEquals("<" + this.tagName + "></" + this.tagName + ">", this.tag.toHtml());
+	}
+
+	@Before
+	public void beforeTesting() {
+		this.tagName = "tag";
+		this.tag = new Tag(this.tagName);
+	}
+
+	@Test
+	public void emptyAttrsGenerateSimpleTag() {
+		this.tag.classCss("").dataAttr("", "").dir("").id("").lang("").style("").tabindex(null).title("");
+		assertEquals("<" + this.tagName + "></" + this.tagName + ">", this.tag.toHtml());
+	}
+
+	@Test
+	public void fullAttrsBuildedInOrder() {
+		this.tag.classCss("test").dataAttr("test", "val").dir("#").id("#").lang("#").style("border: none;").tabindex(1)
+				.title("test");
+
+		assertEquals("<" + this.tagName
+				+ " class=\"test\" data-test=\"val\" dir=\"#\" id=\"#\" lang=\"#\" style=\"border: none;\" "
+				+ "tabindex=\"1\" title=\"test\"></" + this.tagName + ">", this.tag.toHtml());
+	}
+
+	@Test
+	public void prependInvalidValuesGenerateSimpleTag() {
+
+		String string = null;
+		this.tag.prepend(string);
+		this.tag.prepend("");
+
+		Element<?> element = null;
+		this.tag.prepend(element);
+		this.tag.prependTo(element);
+
+		assertEquals("<" + this.tagName + "></" + this.tagName + ">", this.tag.toHtml());
+	}
+
+	@Test
+	public void testAppendAndPrepend() {
+		this.tag.append("3").prepend("2").append("4").prepend("1").append("5");
+		assertEquals("<" + this.tagName + ">12345</" + this.tagName + ">", this.tag.toHtml());
+	}
+
+	@Test
+	public void testAppendTo() {
+		Tag outter = new Tag("outter");
+
+		outter.append("content");
+		new Tag("inner").appendTo(outter);
+
+		assertEquals("<outter>content<inner></inner></outter>", outter.toHtml());
+	}
+
+	@Test
+	public void testPrependTo() {
+		Tag outter = new Tag("outter");
+
+		outter.append("content");
+		new Tag("inner").prependTo(outter);
+
+		assertEquals("<outter><inner></inner>content</outter>", outter.toHtml());
 	}
 }
