1d0a7853270492aa2e88aebed05a81beedf94dbd
Rodrigo Moutinho <rcm1989@gmail.com>
2017-02-05 08:10:09 -0200

    More unit tests for Element

--- src/test/java/br/com/rcmoutinho/javatohtml/core/ElementTest.java
+++ src/test/java/br/com/rcmoutinho/javatohtml/core/ElementTest.java
@@ -1,6 +1,8 @@
 package br.com.rcmoutinho.javatohtml.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +26,7 @@ public class ElementTest {
 	public void addingDataAttrInOrder() {
 
 		this.tag.dataAttr("test1", "value1").dataAttr("test2", "value2");
+		assertFalse(this.tag.dataAttr().isEmpty());
 
 		String html = "<" + this.tagName + " data-test1=\"value1\" data-test2=\"value2\"></" + this.tagName + ">";
 		assertEquals(html, this.tag.toHtml());
@@ -51,17 +54,26 @@ public class ElementTest {
 
 	@Test
 	public void emptyAttrsGenerateSimpleTag() {
-		this.tag.classCss("").dataAttr("", "").dir("").id("").lang("").style("").tabindex(null).title("");
+		this.tag.classCss("").dataAttr("", "").dir("").id("").lang("").style("").style("", "").tabindex(null).title("");
+
+		assertTrue(this.tag.classCss().isEmpty());
+		assertTrue(this.tag.dataAttr().isEmpty());
+		assertTrue(this.tag.style().isEmpty());
+
 		assertEquals("<" + this.tagName + "></" + this.tagName + ">", this.tag.toHtml());
 	}
 
 	@Test
 	public void fullAttrsBuildedInOrder() {
-		this.tag.classCss("test").dataAttr("test", "val").dir("#").id("#").lang("#").style("border: none;").tabindex(1)
-				.title("test");
+		this.tag.classCss("test").dataAttr("test", "val").dir("#").id("#").lang("#").style("border: none;")
+				.style("margin", "2px").tabindex(1).title("test");
+
+		assertFalse(this.tag.classCss().isEmpty());
+		assertFalse(this.tag.dataAttr().isEmpty());
+		assertFalse(this.tag.style().isEmpty());
 
 		assertEquals("<" + this.tagName
-				+ " class=\"test\" data-test=\"val\" dir=\"#\" id=\"#\" lang=\"#\" style=\"border: none;\" "
+				+ " class=\"test\" data-test=\"val\" dir=\"#\" id=\"#\" lang=\"#\" style=\"border: none; margin: 2px;\" "
 				+ "tabindex=\"1\" title=\"test\"></" + this.tagName + ">", this.tag.toHtml());
 	}
 
@@ -96,6 +108,16 @@ public class ElementTest {
 	}
 
 	@Test
+	public void testInvalidClass() {
+		this.tag.classCss(new String[] {});
+
+		String[] array = null;
+		this.tag.classCss(array);
+
+		assertTrue(this.tag.classCss().isEmpty());
+	}
+
+	@Test
 	public void testPrependTo() {
 		Tag outter = new Tag("outter");
 
@@ -103,5 +125,11 @@ public class ElementTest {
 		new Tag("inner").prependTo(outter);
 
 		assertEquals("<outter><inner></inner>content</outter>", outter.toHtml());
+	}
+
+	@Test
+	public void testToStringSameAsToHtmlWithoutArgs() {
+		this.tag.append("test").classCss("class").dataAttr("test", "val").style("border", "none");
+		assertEquals(this.tag.toString(), this.tag.toHtml());
 	}
 }
