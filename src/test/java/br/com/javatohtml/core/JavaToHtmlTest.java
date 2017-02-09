bebacd7465ae05696f1017506897a7f1931fdf38
Rodrigo Moutinho <rcm1989@gmail.com>
2017-02-09 07:54:02 -0200

    Unit tests

---
+++ src/test/java/br/com/javatohtml/core/JavaToHtmlTest.java
@@ -0,0 +1,13 @@
+package br.com.javatohtml.core;
+
+/**
+ * Unit test for {@link JavaToHtml}.
+ * 
+ * @rcmoutinho
+ * @author rodrigo.moutinho
+ * @date 8 de fev de 2017
+ * @email rcm1989@gmail.com
+ */
+public class JavaToHtmlTest {
+
+}
